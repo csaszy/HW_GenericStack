30a5fde219018a48d7e7e85b8815e0a9a63882be
csaszy <csaszy2006@gmail.com>
2024-11-13 19:55:03 +0100

    Add project files.

---
+++ GenericStack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_GenericStack
+{
+    public class GenericStack<T>
+    {
+        private List<T> elements = new List<T>();
+        public void Push(T item)
+        {
+            elements.Add(item);
+        }
+        public T Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+            T item = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            return item;
+        }
+        public T Peek()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+            return elements[elements.Count - 1];
+        }
+        public bool IsEmpty()
+        {
+            return elements.Count == 0;
+        }
+
+        public Stack<T> slice(int start, int end)
+        {
+            //handling invalid input
+            if(end <= start) throw new InvalidOperationException("End must be bigger than start");
+            if(end < 0 || end >= this.elements.Count) throw new InvalidOperationException("End is out of range");
+            if(start < 0 || start >= this.elements.Count) throw new InvalidOperationException("Start is out of range");
+
+            //main stuff
+            var stack = new Stack<T>();
+            for (int i = start; i < end; i++)
+            {
+                stack.Push(elements[i]);
+            }
+            return stack;
+        }
+        public GenericStack<T> splice(int start, int count, IEnumerable<T> items)
+        {
+            var stack = new GenericStack<T>();
+            this.elements.ForEach(e => { stack.Push(e); }); 
+            stack.elements.RemoveRange(start, count);
+            stack.elements.InsertRange(start, items);
+            return stack;
+        }
+        public T shift()
+        {
+            if (this.IsEmpty()) throw new Exception();
+
+            T temp = this.elements[0];
+            this.elements.RemoveAt(0);
+            return temp;
+        }
+        public void unshift(T item)
+        {
+            this.elements.Insert(0, item);
+        }
+
+        //for debugging purposes
+        public override string ToString()
+        {
+            return string.Join("\t", this.elements);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_GenericStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericStack<int> genericStack = new GenericStack<int>();
+            for(int i = 0; i < 10; i++) genericStack.Push(i);    
+
+            Console.WriteLine($"Base: \n{genericStack.ToString()}\n");
+
+            //1.feladat
+            var slicedStack = genericStack.slice(1, 4);
+            Console.WriteLine($"1.feladat: \n{string.Join("\t", slicedStack)}\n");
+
+            //2.feladat
+            var splicedStack = genericStack.splice(2, 3, new int[]{9,9,9});
+            Console.WriteLine($"2.feladat: \n{splicedStack.ToString()}\n");
+
+            //3.feladat
+            Console.WriteLine($"3.feladat: \n{genericStack.shift()}\n{genericStack.ToString()}\n");
+
+            //4.feladat
+            genericStack.unshift(9);
+            Console.WriteLine($"4.feladat: \n{genericStack.ToString()}\n");
+        }
+    }
+}
